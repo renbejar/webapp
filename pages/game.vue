476360f5c6210844cf9ecadade163ac9e14be1dc
renbejar <jaredrenberg623@gmail.com>
2022-10-18 18:42:13 -0600

    adding files

---
+++ pages/game.vue
@@ -0,0 +1,32 @@
+<script>
+let id = 0;
+
+export default {
+  data() {
+    return {
+      word: "",
+      words: [],
+    };
+  },
+  methods: {
+    addword() {
+      this.words.push({ id: id++, text: this.newWord });
+      this.newWord = "";
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="addword">
+      <input v-model="newWord" />
+      <button>Guess Word</button>
+    </form>
+    <ul>
+      <li v-for="word in words" :key="word.id">
+        {{ word.text }}
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Welcome to the game!</h1>
+    <p>
+      You will recieve scrambled letters from a random word.
+      <br />
+      Unscrable the word and you win! Click the button to continue.
+    </p>
+    <button onclick="location.href='game'" type="button">Start the game</button>
+  </div>
+</template>
